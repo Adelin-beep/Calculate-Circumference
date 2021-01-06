8f1e8412df2d36307903819893f61b081270abee
Adelin-beep <73435099+Adelin-beep@users.noreply.github.com>
2021-01-06 23:24:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/** ==Algoritma==
+1. Masukkan nilai pi (Math.PI = 3.14)
+2. Masukkan nilai jariJari
+3. Siapkan penampung untuk menghitung nilai keliling
+4. Hitung keliling dengan cara 2 times pi times jariJari
+5. Tampilkan nilai keliling
+*/
+
+
+/** ===Pseudocode===
+STORE pi WITH Math.PI
+STORE jariJari WTIH ANY NUMBER
+STORE keliling WITH ANY NUMBER
+SET keliling with (2 TIMES pi TIMES jariJari)
+DISPLAY "keliling : " CONCAT WITH keliling
+*/
+
+// Insert your code here
+var pi = Math.PI;
+var jariJari;
+var jariJari= 10;
+var keliling = 2*pi*jariJari;
+
+console.log(keliling);
